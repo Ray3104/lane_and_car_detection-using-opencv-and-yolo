4d8cb912694c892a2e32d6575b1535f923d1ed71
Ray3104 <82972870+Ray3104@users.noreply.github.com>
2021-05-24 12:53:01 +0200

    Update lane.py

--- lane.py
+++ lane.py
@@ -555,7 +555,7 @@ class Lane:
     s_channel = hls[:, :, 2] # use only the saturation channel data
     #_, s_binary = edge.threshold(s_channel, (130, 255))
     #_, s_binary = edge.threshold(s_channel, (80, 255))
-    _, s_binary = edge.threshold(s_channel, (200, 255))
+    _, s_binary = edge.threshold(s_channel, (20, 255))
 	
     # Perform binary thresholding on the R (red) channel of the 
 		# original BGR video frame. 
